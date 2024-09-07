75ca619089e08def1e5bf94a26bbc9196c5b7cbc
Rajkumar Mandal <106177834+RajMandal17@users.noreply.github.com>
2024-09-07 22:00:10 +0530

    Time: 0 ms (100%), Space: 41.2 MB (26.7%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.java
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.java
@@ -5,7 +5,7 @@ class Solution {
         
         for (int num : nums) {
             if (freq.containsKey(num)) {
-                // If num has been seen before, the number of good pairs is increased by the frequency count of that num
+                
                 count += freq.get(num);
                 // Increment the frequency count of the current num
                 freq.put(num, freq.get(num) + 1);
